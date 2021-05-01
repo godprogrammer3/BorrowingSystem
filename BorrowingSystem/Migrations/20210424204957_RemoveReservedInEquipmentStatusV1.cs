33594301fc993db6f65a15aa5500b9ff90b84963
goprogrammer <game47you@gmail.com>
2021-05-01 14:30:37 +0700

    remove home controller

---
+++ BorrowingSystem/Migrations/20210424204957_RemoveReservedInEquipmentStatusV1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BorrowingSystem.Migrations
+{
+    public partial class RemoveReservedInEquipmentStatusV1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- BorrowingSystem/Models/Equipment.cs
+++ BorrowingSystem/Models/Equipment.cs
@@ -12,7 +12,6 @@ namespace BorrowingSystem.Models
         public enum EquipmentStatus
         {
             available,
-            reserved,
             repairing
         }
         [Key]
