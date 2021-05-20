4d30dee2bd8541c71f267b1a06321c13076d4f3f
goprogrammer <game47you@gmail.com>
2021-05-21 05:29:28 +0700

    Add Room Detail section in dashboard

--- BorrowingSystem/wwwroot/js/Dashboard.js
+++ BorrowingSystem/wwwroot/js/Dashboard.js
@@ -1,4 +1,6 @@
-﻿window.onload = function () {
+﻿
+
+window.onload = function () {
     initialMainRoom();
 }
 
@@ -52,7 +54,10 @@ async function initialMainRoom(event) {
 }
 
 async function initialRoomDetail(room) {
-    var result = new Promise((resolve,reject) => {
+    document.getElementById('roomDetailAvailableSectionOnLoading').style.display = 'block';
+    document.getElementById('roomDetailAvailableSectionOnSuccess').style.display = 'none';
+    document.getElementById('roomDetailAvailableSectionOnError').style.display = 'none';
+    var result = await  new Promise((resolve,reject) => {
         try {
             getAvailableEquipmentInMonth(room.id, resolve);
         } catch (error) {
@@ -62,21 +67,65 @@ async function initialRoomDetail(room) {
     });
     if (result) {
         var days = JSON.parse(result.body);
-        console.log(days);
         let insideContent;
         if (days.length > 0) {
-            sideContent = `<h3>Succeess.</h3>`
+            insideContent = `
+                <span>${room.name}</span>
+                <span>${room.equipmentName}</span>
+                <h4>${ new Date().toLocaleDateString("en-US") }</h4>
+                <section style="display:flex;">
+            `;
+            var nowDate = new Date();
+            nowDate = nowDate.getDate();
+            var firstDateOfWeek = nowDate - days[nowDate - 1].dayOfWeek + 1;
+            for (var countDay = 0; countDay < 7; countDay++) {
+                insideContent += `<span><section><p>`;
+                insideContent += dayOfWeekToString(days[firstDateOfWeek-1].dayOfWeek) + '</p>';
+                insideContent += `<p>${firstDateOfWeek}</p>`;
+                insideContent += `</section></span>`;
+                firstDateOfWeek++;  
+            }
+            insideContent += '</section>'
+            insideContent += `<p>Today</p>`
+            insideContent += `<section><ul style="list-style-type:none;" >`;
+            for (var timeIndex = 9; timeIndex <= 21; timeIndex++) {
+                insideContent += '<li>'
+                insideContent += `<span>${(timeIndex == 9) ? '09' : timeIndex}:00-${timeIndex + 1}:00</span>`;
+                insideContent += `<span style="margin-left:10px;">${availableQuantityToString( days[nowDate - 1].availableEquipments[timeIndex-9].quantity ) }</span>`;
+                insideContent += '</li>'
+            }
+            insideContent += `</section></ul>`;
         } else {
             insideContent = `<h3>Error Empty content !</h3>`
         }
-        document.getElementById('roomDetailSectionOnSuccess').innerHTML = insideContent;
-        document.getElementById('roomDetailSectionOnLoading').style.display = 'none';
-        document.getElementById('roomDetailSectionOnSuccess').style.display = 'block';
-        document.getElementById('roomDetailSectionOnError').style.display = 'none';
+        document.getElementById('roomDetailAvailableSectionOnSuccess').innerHTML = insideContent;
+        document.getElementById('roomDetailAvailableSectionOnLoading').style.display = 'none';
+        document.getElementById('roomDetailAvailableSectionOnSuccess').style.display = 'block';
+        document.getElementById('roomDetailAvailableSectionOnError').style.display = 'none';
     } else {
-        document.getElementById('roomDetailSectionOnLoading').style.display = 'none';
-        document.getElementById('roomDetailSectionOnSuccess').style.display = 'none';
-        document.getElementById('roomDetailSectionOnError').style.display = 'block';
+        document.getElementById('roomDetailAvailableSectionOnLoading').style.display = 'none';
+        document.getElementById('roomDetailAvailableSectionOnSuccess').style.display = 'none';
+        document.getElementById('roomDetailAvailableSectionOnError').style.display = 'block';
     }
 }
 
+function dayOfWeekToString( dayOfWeek ) {
+    var listDays = [        
+        'Su',
+        'Mo',
+        'Tu',
+        'We',
+        'Th',
+        'Fr',
+        'Sa'
+    ];
+    return listDays[dayOfWeek];
+}
+
+function availableQuantityToString( quantity ) {
+    if (quantity == 0) {
+        return 'Reservation is full';
+    } else {
+        return quantity + ' available';
+    }
+}
